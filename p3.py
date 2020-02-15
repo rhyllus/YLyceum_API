637f046dca8ed1ee8ffecac020b8bd0f27cd5f5e
aleks pavlushina <aleks.pavlushina@yandex.ru>
2020-02-15 11:03:36 +0300

    задача 3 (не доработано)

--- p3.py
+++ p3.py
@@ -3,10 +3,11 @@ import sys
 
 import pygame
 import requests
-def openn(x, y, m1, m2, format_='map'):
 
+def openn(x, y, m1, m2, format_='map'):
     response = None
-    map_request = "http://static-maps.yandex.ru/1.x/?ll={},{}&spn={},{}&l={}".format(str(x), str(y), str(m1), str(m2), format_)
+    map_request = "http://static-maps.yandex.ru/1.x/?ll={},{}&spn={},{}&l={}".format(
+        str(x), str(y), str(m1), str(m2), format_)
     response = requests.get(map_request)
 
     if not response:
@@ -15,21 +16,37 @@ def openn(x, y, m1, m2, format_='map'):
         print("Http статус:", response.status_code, "(", response.reason, ")")
         sys.exit(1)
 
-
     map_file = "map.png"
     with open(map_file, "wb") as file:
         file.write(response.content)
 
-
     pygame.init()
     screen = pygame.display.set_mode((600, 450))
-
     screen.blit(pygame.image.load(map_file), (0, 0))
-
     pygame.display.flip()
-    while pygame.event.wait().type != pygame.QUIT:
-        pass
-    pygame.quit()
-
     os.remove(map_file)
-openn(37.57, 55.703118, 0.005, 0.00111)
+
+
+x = float(input())
+y = float(input())
+m1 = float(input())
+m2 = float(input())
+f = input()
+openn(x, y, m1, m2, format_=f)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            key = pygame.key.get_pressed()
+            if key[pygame.K_RIGHT]:
+                x += 1
+            elif key[pygame.K_LEFT]:
+                x -= 1
+            elif key[pygame.K_UP]:
+                y += 1
+            elif key[pygame.K_DOWN]:
+                y -= 1
+    openn(x, y, m1, m2, format_='map')
